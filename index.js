1b3c447d6340664164ca898ae9d14340bf934b7d
safasf2fsd <gs1mrman127@hotmail.com>
2024-05-07 10:32:14 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function minMeetingRoomsII(intervals) {
-  const startTimes = intervals
-    .map((interval) => interval[0])
-    .sort((a, b) => a - b);
-  const endTimes = intervals
-    .map((interval) => interval[1])
-    .sort((a, b) => a - b);
-  let rooms = 0;
-  let endIdx = 0;
-  for (let i = 0; i < startTimes.length; i++) {
-    if (startTimes[i] < endTimes[endIdx]) rooms++;
-    else endIdx++;
-  }
-  return rooms;
-}
+const maxN = (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n);
