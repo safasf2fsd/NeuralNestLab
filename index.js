6eb0288d91e1e8cd2253f45c0a1dfb3781f39bd9
safasf2fsd <gs1mrman127@hotmail.com>
2024-05-08 13:31:35 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const maxN = (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n);
+function numIslands(grid) {
+  if (!grid || grid.length === 0) return 0;
+  const m = grid.length;
+  const n = grid[0].length;
+  let islands = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === "1") {
+        islands++;
+        dfs(grid, i, j);
+      }
+    }
+  }
+  return islands;
+}
+function dfs(grid, i, j) {
+  const m = grid.length;
+  const n = grid[0].length;
+  if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") return;
+  grid[i][j] = "0";
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
+}
