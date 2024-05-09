3d500f39a152960f91fc78bfcd5f2259190ce534
safasf2fsd <gs1mrman127@hotmail.com>
2024-05-10 05:21:12 +0700

    init project

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-function numIslands(grid) {
-  if (!grid || grid.length === 0) return 0;
-  const m = grid.length;
-  const n = grid[0].length;
-  let islands = 0;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (grid[i][j] === "1") {
-        islands++;
-        dfs(grid, i, j);
+function maximalSquare(matrix) {
+  if (matrix.length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from(Array(rows + 1), () => Array(cols + 1).fill(0));
+  let maxSide = 0;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+        maxSide = Math.max(maxSide, dp[i][j]);
       }
     }
   }
-  return islands;
-}
-function dfs(grid, i, j) {
-  const m = grid.length;
-  const n = grid[0].length;
-  if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") return;
-  grid[i][j] = "0";
-  dfs(grid, i + 1, j);
-  dfs(grid, i - 1, j);
-  dfs(grid, i, j + 1);
-  dfs(grid, i, j - 1);
+  return maxSide * maxSide;
 }
