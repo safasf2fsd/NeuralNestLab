658f419b2d3299790dd4d3b62afc843b7280b9b9
safasf2fsd <gs1mrman127@hotmail.com>
2024-05-14 06:34:52 +0700

    demo

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+function minimumTotal(triangle) {
+  const n = triangle.length;
+  const dp = new Array(n + 1).fill(0);
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = 0; j <= i; j++) {
+      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
+    }
+  }
+  return dp[0];
+}
